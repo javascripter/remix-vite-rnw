362668ab3977e45f265ed950e61a1ab529ec640d
Tsubasa Sakai <javascripter@tsukkun.net>
2024-03-06 16:00:24 +0700

    deploy to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
+
+import { vercelPreset } from "@vercel/remix/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  // For Vercel deployment, we need to use the Vercel preset:
+  // https://github.com/vercel/vercel/pull/11031
+  plugins: [remix({ presets: [vercelPreset()] }), tsconfigPaths()],
 });
